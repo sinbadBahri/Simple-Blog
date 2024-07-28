852371918b4e23e5772627fb8358db4a7beef5f7
sinbadBahri <h.l.kh7990@gmail.com>
2024-07-28 13:41:50 +0330

    Middleware EnsureUserIsAdminOrManager added.

---
+++ blog/app/Http/Middleware/EnsureUserIsAdminOrManager.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Role;
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsAdminOrManager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {   
+        if (Auth::check()) {
+            $admin_role = Role::where('name', 'admin')->first();
+            $registered_user = User::with('roles')->find(Auth::user()->id);
+            if (in_array($admin_role, $registered_user->roles)) {
+                
+                return $next($request);
+            }
+        }
+        return redirect(to:'/login');
+    }
+}
