83140ab171c51a66e687a83a674952796ab98148
sinbadBahri <h.l.kh7990@gmail.com>
2024-07-28 11:28:00 +0330

    Now when a user registers, the default role 'member' will difine for the users profile and permissions.

--- blog/app/Http/Controllers/Auth/RegisteredUserController.php
+++ blog/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -40,6 +41,9 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $role = Role::where('name', 'member')->first();
+        $user->roles()->attach($role->id);
 
         event(new Registered($user));
 
