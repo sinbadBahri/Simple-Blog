2c4753e9a72dce1dba26667a37301ad5adbea67a
sinbadBahri <h.l.kh7990@gmail.com>
2024-07-28 11:07:07 +0330

    Hekmatinasser package for jalali date added

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
     </head>
     <body class="font-sans antialiased">
+        @yield('content')
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
--- blog/resources/views/users/index.blade.php
+++ blog/resources/views/users/index.blade.php
@@ -1,7 +1,7 @@
 <x--app-layout>
 
-
   <body>
+    @section('content')
     @foreach ($users as $user)
       <table class="table">
         <thead>
@@ -9,20 +9,23 @@
             <th scope="col">#id</th>
             <th scope="col">Name</th>
             <th scope="col">Email</th>
+            <th scope="col">Created Date</th>
           </tr>
         </thead>
         <tbody class="table-group-divider">
           
             <tr>
               <th scope="row">{{ $user->id }}</th>
-              <td>{{ $user->name }}</td>
+              <th>{{ $user->name }}</th>
               <td>{{ $user->email }}</td>
+              <td>{{ Hekmatinasser\Verta\Verta::instance($user->created_at)->formatWord('l dS F') }}</td>
             </tr>
         </tbody>
       </table>
   
               
     @endforeach  
+    {{-- @endsection --}}
   </body>
 
 
