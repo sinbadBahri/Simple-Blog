17b2343007ea05adc474fe88d383235283f107c2
sinbadBahri <h.l.kh7990@gmail.com>
2024-07-30 15:28:27 +0330

    Bugs fixed

--- blog/resources/views/users/index.blade.php
+++ blog/resources/views/users/index.blade.php
@@ -1,7 +1,17 @@
 <x-app-layout>
 
-  <body>
-    @section('content')
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Users List') }}
+    </h2>
+</x-slot>
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+                
+              @section('content')
     @foreach ($users as $user)
       <table class="table">
         <thead>
@@ -36,8 +46,13 @@
   
               
     @endforeach  
-    {{-- @endsection --}}
-  </body>
+              
+             
+            </div>
+        </div>
+    </div>
+</div>
+
 
 
 
